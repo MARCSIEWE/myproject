f3b73f05bb87197c38f65ab7311d4d1992f1b6d3
MARCSIEWE <marcaurele235@gmail.com>
2023-07-06 07:55:14 -0900

    third commit

--- bootstrap cv2/app.js
+++ bootstrap cv2/app.js
@@ -1,12 +1,36 @@
-const observe = new IntersectionObserver((entries)=>{
-    for(let entry of entries){
-        if(entry.isIntersecting){
-            entry.target.classList.add('reveal-visible')
-        }
-    }
+const ratio = .1
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: ratio
+}
 
+const handleIntersect = function (entries, observe) {
+    entries.forEach(function (entry) {
+        if(entry.isIntersecting > ratio){
+            entry.target.classList.add('reveal-visible')
+            observe.unobserve(entry.target)
+        }
+    })
+}
+
+const handleIntersect2 = function (entries, observe) {
+    entries.forEach(function (entry) {
+        if(entry.isIntersecting > ratio){
+            entry.target.classList.add('visible')
+            observe.unobserve(entry.target)
+        }
+    })
+}
+
+const observe2 = new IntersectionObserver(handleIntersect2, options)
+
+const observe = new IntersectionObserver(handleIntersect, options)
+
+document.querySelectorAll('[class*="reveal-"]').forEach(function (reveal) {
+    observe.observe(reveal)
 })
 
-document.querySelectorAll('[class*="reveal-"]').forEach((reveal)=>{
-    observe.observe(reveal)
+document.querySelectorAll('[class*="reveal2-"]').forEach(function (r) {
+    observe2.observe(r)
 })
